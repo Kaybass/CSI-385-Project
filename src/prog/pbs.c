21fb0ef99df5f139ce3467afb26100744b626c3d
Alexander Bean Apmann <caboose200@gmail.com>
2016-03-19 21:54:46 -0400

    pbs kids

---
+++ src/prog/pbs.c
@@ -0,0 +1,173 @@
+/******************************************************************************
+ * main: Sample for starting the FAT project.
+ *
+ * Authors:  Andy Kinley, Archana Chidanandan, David Mutchler and others.
+ *           March, 2004.
+ *****************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+// 13 is NOT the correct number -- you fix it!
+#define BYTES_TO_READ_IN_BOOT_SECTOR 62
+
+/******************************************************************************
+ * You must set these global variables:
+ *    FILE_SYSTEM_ID -- the file id for the file system (here, the floppy disk
+ *                      filesystem)
+ *    BYTES_PER_SECTOR -- the number of bytes in each sector of the filesystem
+ *
+ * You may use these support functions (defined in FatSupport.c)
+ *    read_sector
+ *    write_sector
+ *    get_fat_entry
+ *    set_fat_entry
+ *****************************************************************************/
+
+FILE* FILE_SYSTEM_ID;
+int BYTES_PER_SECTOR;
+
+extern int read_sector(int sector_number, char* buffer);
+
+/******************************************************************************
+ * main: an example of reading an item in the boot sector
+ *****************************************************************************/
+
+int main(int argc, char *argv[])
+{
+   unsigned char* boot;            // example buffer
+
+   int mostSignificantBits;
+   int leastSignificantBits;
+   int bytesPerSector;
+   int sectorsPerCluster;
+   int reservedSectors;
+   int numberFats;
+   int maxRootEntries;
+   int TSC;
+   int sectorsPerFat;
+   int sectorsPerTrack;
+   int numberHeads;
+   int one,two,three,four;
+   unsigned char bootSignature;
+   unsigned int volumeId;
+   unsigned char volumeLabel[11], fileSystemType[8];
+
+   // You must set two global variables for the disk access functions:
+   //      FILE_SYSTEM_ID         BYTES_PER_SECTOR
+
+   // Use this for an image of a floppy drive
+
+
+   if(argc == 1){
+       printf("baka\n");
+       exit(1);
+   }
+   else if(argc >= 2){
+       if(strcmp(argv[1],"-h") == 0){
+           printf("One argument, file\n");
+           exit(1);
+       }
+       FILE_SYSTEM_ID = fopen(argv[1], "r+");
+       if (FILE_SYSTEM_ID == NULL)
+       {
+          printf("Could not open the floppy drive or image.\n");
+          exit(1);
+       }
+   }
+
+   // Set it to this only to read the boot sector
+   BYTES_PER_SECTOR = BYTES_TO_READ_IN_BOOT_SECTOR;
+
+   // Then reset it per the value in the boot sector
+
+   boot = (unsigned char*) malloc(BYTES_PER_SECTOR * sizeof(unsigned char));
+
+   if (read_sector(0, boot) == -1)
+      printf("Something has gone wrong -- could not read the boot sector\n");
+
+
+   // 12 (not 11) because little endian
+   mostSignificantBits  = ( ( (int) boot[12] ) << 8 ) & 0x0000ff00;
+   leastSignificantBits =   ( (int) boot[11] )        & 0x000000ff;
+   bytesPerSector = mostSignificantBits | leastSignificantBits;
+
+   sectorsPerCluster = (unsigned int) boot[13];
+
+   mostSignificantBits  = ( ( (int) boot[15] ) << 8 ) & 0x0000ff00;
+   leastSignificantBits =   ( (int) boot[14] )        & 0x000000ff;
+   reservedSectors = mostSignificantBits | leastSignificantBits;
+
+   numberFats = (unsigned int) boot[16];
+
+
+   mostSignificantBits  = ( ( (int) boot[18] ) << 8 ) & 0x0000ff00;
+   leastSignificantBits =   ( (int) boot[17] )        & 0x000000ff;
+   maxRootEntries = mostSignificantBits | leastSignificantBits;
+
+   mostSignificantBits  = ( ( (int) boot[20] ) << 8 ) & 0x0000ff00;
+   leastSignificantBits =   ( (int) boot[19] )        & 0x000000ff;
+   TSC = mostSignificantBits | leastSignificantBits;
+
+   mostSignificantBits  = ( ( (int) boot[23] ) << 8 ) & 0x0000ff00;
+   leastSignificantBits =   ( (int) boot[22] )        & 0x000000ff;
+   sectorsPerFat = mostSignificantBits | leastSignificantBits;
+
+   mostSignificantBits  = ( ( (int) boot[25] ) << 8 ) & 0x0000ff00;
+   leastSignificantBits =   ( (int) boot[24] )        & 0x000000ff;
+   sectorsPerTrack = mostSignificantBits | leastSignificantBits;
+
+   mostSignificantBits  = ( ( (int) boot[27] ) << 8 ) & 0x0000ff00;
+   leastSignificantBits =   ( (int) boot[26] )        & 0x000000ff;
+   numberHeads = mostSignificantBits | leastSignificantBits;
+
+   bootSignature = boot[38];
+
+   one = ( ( (int) boot[42] ) << 24 ) & 0xff000000;
+   two = ( ( (int) boot[41] ) << 16 ) & 0x00ff0000;
+   three = ( ( (int) boot[40] ) << 8 ) & 0x0000ff00;
+   four = ( ( (int) boot[39] ) ) & 0x000000ff;
+
+   volumeId = one | two | three | four;
+
+   for(int i = 0; i < 11; i++){
+       volumeLabel[i] = boot[43+i];
+   }
+
+   for(int i = 0; i < 8; i++){
+       fileSystemType[i] = boot[54+i];
+   }
+
+   printf("Bytes per sector = %d\n"
+            "Sectors per cluster = %d\n"
+            "Number of FATs = %d\n"
+            "Number of reserved sectors = %d\n"
+            "Number of root entries = %d\n"
+            "Total sector count = %d\n"
+            "Sectors per FAT = %d\n"
+            "Sectors per track = %d\n"
+            "Number of heads = %d\n"
+            "Boot signature (in hex) = 0x%x\n"
+            "Volume ID (in hex) = 0x%8x\n"
+            "Volume label = %s\n"
+            "File system type = %s\n",
+             bytesPerSector,
+             sectorsPerCluster,
+             numberFats,
+             reservedSectors,
+             maxRootEntries,
+             TSC,
+             sectorsPerFat,
+             sectorsPerTrack,
+             numberHeads,
+             ((unsigned int)bootSignature),
+             volumeId,
+             volumeLabel,
+             fileSystemType);
+
+   free(boot);
+
+   return 0;
+}
--- src/utils/fatSupport.h
+++ src/utils/fatSupport.h
@@ -1,7 +1,11 @@
+
+#ifndef FAT_SUPPORT
+#define FAT_SUPPORT
+
 int read_sector(int sector_number, char* buffer);
 int write_sector(int sector_number, char* buffer);
 
 unsigned int get_fat_entry(int fat_entry_number, char* fat);
 void set_fat_entry(int fat_entry_number, int value, char* fat);
 
-
+#endif
