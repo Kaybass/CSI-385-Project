8b59844cfcddf7df38cf636c13a12c574124ca02
Ethan Lamitie <elamitie@gmail.com>
2016-04-30 18:15:41 -0400

    Made ls sector reading great again (I think)

--- src/prog/ls.c
+++ src/prog/ls.c
@@ -120,47 +120,64 @@ int main(int argc, char **argv)
         }
     }
     else{
-        image = (byte*)malloc(BYTES_PER_SECTOR * sizeof(ubyte));
-        read_sector(19,image);
+        // Read in sectors used in Root
+        int done = 0; 
+        int sec = 19;
+        while (done != 1) {
+            image = (byte*)malloc(BYTES_PER_SECTOR * sizeof(ubyte));
+            read_sector(sec, image);
 
-        for(int i = 0; i < 16; i++){
-            for(int j = 0; j < 8; j++){
-                files[i].Filename[j] = image[j + i * 32];
+            for(int i = 0; i < 16; i++){
+
+                if (image[i * 32] == 0x00) {
+                    done = 1;
+                    break;
+                }
+
+                for(int j = 0; j < 8; j++){
+                    files[i].Filename[j] = image[j + i * 32];
+                }
+                files[i].Filename[8] = '\0';
+
+                for(int j = 0; j < 3; j++){
+                    files[i].Type[j] = image[j + 8 + i  * 32];
+                }
+                files[i].Type[3] = '\0';
+
+                files[i].Attributes = image[11 + i  * 32];
+
+                files[i].CreationTime[0] = image[14 + i * 32];
+                files[i].CreationTime[1] = image[15 + i * 32];
+
+                files[i].CreationDate[0] = image[16 + i * 32];
+                files[i].CreationDate[1] = image[17 + i * 32];
+
+                files[i].LastAccessDate[0] = image[18 + i * 32];
+                files[i].LastAccessDate[1] = image[19 + i * 32];
+
+                files[i].LastWriteTime[0] = image[22 + i * 32];
+                files[i].LastWriteTime[1] = image[23 + i * 32];
+
+                files[i].LastWriteDate[0] = image[24 + i * 32];
+                files[i].LastWriteDate[1] = image[25 + i * 32];
+
+                h = ( ( (int) image[27 + i  * 32] ) << 8 ) & 0x0000ff00;
+                l =   ( (int) image[26 + i  * 32] )        & 0x000000ff;
+                files[i].FirstLogicalCluster = h | l;
+
+                h =   ( ( (int) image[31 + i * 32] ) << 24 ) & 0xff000000;
+                l =   ( ( (int) image[30 + i * 32] ) << 16 ) & 0x00ff0000;
+                j =   ( ( (int) image[29 + i * 32] ) << 8  ) & 0x0000ff00;
+                k =   (   (int) image[28 + i * 32] )         & 0x000000ff;
+                files[i].FileSize = h | l | j | k;
             }
-            files[i].Filename[8] = '\0';
 
-            for(int j = 0; j < 3; j++){
-                files[i].Type[j] = image[j + 8 + i  * 32];
-            }
-            files[i].Type[3] = '\0';
-
-            files[i].Attributes = image[11 + i  * 32];
-
-            files[i].CreationTime[0] = image[14 + i * 32];
-            files[i].CreationTime[1] = image[15 + i * 32];
-
-            files[i].CreationDate[0] = image[16 + i * 32];
-            files[i].CreationDate[1] = image[17 + i * 32];
-
-            files[i].LastAccessDate[0] = image[18 + i * 32];
-            files[i].LastAccessDate[1] = image[19 + i * 32];
-
-            files[i].LastWriteTime[0] = image[22 + i * 32];
-            files[i].LastWriteTime[1] = image[23 + i * 32];
-
-            files[i].LastWriteDate[0] = image[24 + i * 32];
-            files[i].LastWriteDate[1] = image[25 + i * 32];
-
-            h = ( ( (int) image[27 + i  * 32] ) << 8 ) & 0x0000ff00;
-            l =   ( (int) image[26 + i  * 32] )        & 0x000000ff;
-            files[i].FirstLogicalCluster = h | l;
-
-            h =   ( ( (int) image[31 + i * 32] ) << 24 ) & 0xff000000;
-            l =   ( ( (int) image[30 + i * 32] ) << 16 ) & 0x00ff0000;
-            j =   ( ( (int) image[29 + i * 32] ) << 8  ) & 0x0000ff00;
-            k =   (   (int) image[28 + i * 32] )         & 0x000000ff;
-            files[i].FileSize = h | l | j | k;
+            // Move on to the next sector
+            sec++;
         }
+         
+
+        
         free(image);
     }
 
